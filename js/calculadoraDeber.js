236ddcf514a55435fd1a879d0290165296602a4d
Alfred <anthonysub300@gmail.com>
2023-11-27 23:42:06 -0500

    Deber calculadora: vista del proceso de la operacion y estilo hover

--- js/calculadoraDeber.js
+++ js/calculadoraDeber.js
@@ -3,35 +3,43 @@ let operador = "";
 let numY = [];
 
 const obtenerCantidad = (tmp) =>{ 
-    console.log(tmp)
     if(operador === "" ){
-        if( Number.isInteger(tmp) ) { numX.push(tmp)}
-        else if( typeof tmp === "string") { operador = tmp }
-    }else{
-        if( Number.isInteger(tmp)){ numY.push(tmp)}
-        
-        let x = parseInt( numX.join("") );
-        let y = parseInt(numY.join(""))
-        let resultado=0;
-        switch (operador){
-            case "+": 
-                resultado = x+y;
-            break;
-            case "-": 
-                resultado = x-y;
-            break;
-            case "*": 
-                resultado = x*y;
-            break;
-            case "/": 
-                resultado = x/y;
-            break;
+        if( Number.isInteger(tmp) ) { 
+            numX.push(tmp);
+            document.getElementById('idResultado').innerText = numX.join("");
         }
-        document.getElementById('idResultado').innerText = resultado;
-        
-        if(tmp==='R'){
-            console.log("entró");
+        else if( typeof tmp === "string") { 
+            operador = tmp 
+            document.getElementById('idResultado').innerText += tmp;
+        }
+    }else{
+        if(tmp==='R' || document.getElementById('idResultado').textContent.includes("=") ){
             resetearVariables();
+        }else{
+            if( Number.isInteger(tmp)){ 
+                numY.push(tmp)
+                document.getElementById('idResultado').innerText += tmp;
+            }
+            else if(tmp === '='){
+                let x = parseInt( numX.join("") );
+                let y = parseInt(numY.join(""))
+                let resultado=0;
+                switch (operador){
+                    case "+": 
+                        resultado = x+y;
+                    break;
+                    case "-": 
+                        resultado = x-y;
+                    break;
+                    case "*": 
+                        resultado = x*y;
+                    break;
+                    case "/": 
+                        resultado = x/y;
+                    break;
+                }
+                document.getElementById('idResultado').innerText += " = "+resultado;
+            }
         }
     }
 }
