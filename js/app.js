01ea5ed46b6a89c685ff440be9ba4485fedc4890
Alfred <anthonysub300@gmail.com>
2023-12-10 19:09:20 -0500

    Taller 14: estructura interna Vue. Uso de method y data en Vue

--- js/app.js
+++ js/app.js
@@ -9,6 +9,27 @@ const app = Vue.createApp({
     <p>{{[1,2,3,4,5,6,7,8]}}</p>
     <p>{{true?'verdadero':'falso'}}</p>
     `*/
+    // OPTIONS API
+    methods: {
+        cambiarMensaje(){
+            console.log("Se está cambiando el mensaje")
+            console.log(this.mensaje)
+            this.mensaje='Valor cambiado tal'
+            console.log(this.mensaje);
+        },
+        cambiarNumero(){
+            
+        }
+    },
+    watch: {
+
+    },
+    data(){
+        return{
+            mensaje: 'Hola mundo desde Vue.JS',
+            valor: 100
+        }
+    }
 })
 
 app.mount('#myApp')
